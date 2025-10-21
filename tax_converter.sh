108d2dc40c831cc8db19b5c2e8532371703b8db3
Holosnyiy Artem <artemgolosny@gmail.com>
2025-10-21 10:03:16 +0000

    Додано скрипт tax_converter.sh з повним тестуванням

---
+++ tax_converter.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Використання: $0 <ім'я_файлу.csv>"
+    exit 1
+fi
+
+INPUT_FILE="$1"
+
+gawk '
+BEGIN {
+    FS = ","
+    OFS = ","
+
+    tax_col = 0
+}
+
+NR == 1 {
+    for (i = 1; i <= NF; i++) {
+        col_name = $i
+        gsub(/[[:space:]]+/, "", val)
+        if (tolower(col_name) == "tax") {
+            tax_col = i
+            break
+        }
+    }
+    print $0
+    next
+}
+
+NR > 1 {
+    if (tax_col > 0) {
+        val = $(tax_col)
+        gsub(/[[:space:]]+/, "", val)
+
+        if (val ~ /^([0-9]+(\.[0-9]+)?|\.[0-9]+)$/) {
+            percent = (val + 0) * 100  
+            $(tax_col) = percent "%"
+        }
+        else if (val ~ /^([0-9]+(\.[0-9]+)?|\.[0-9]+)%$/) {
+            $(tax_col) = val
+        }
+        else {
+            $(tax_col) = "N/A"
+        }
+    }
+    print $0
+}
+
+' "$INPUT_FILE"
