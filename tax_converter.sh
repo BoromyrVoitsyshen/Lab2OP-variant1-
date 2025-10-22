6463be63e32ba17915f0a18d434ee321ab05329c
BoromyrVoitsyshen <146740149+BoromyrVoitsyshen@users.noreply.github.com>
2025-10-22 14:17:46 +0300

    Simplify tax conversion script using external awk file

--- tax_converter.sh
+++ tax_converter.sh
@@ -5,46 +5,5 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-INPUT_FILE="$1"
-
-gawk '
-BEGIN {
-    FS = ","
-    OFS = ","
-
-    tax_col = 0
-}
-
-NR == 1 {
-    for (i = 1; i <= NF; i++) {
-        col_name = $i
-        gsub(/[[:space:]]+/, "", val)
-        if (tolower(col_name) == "tax") {
-            tax_col = i
-            break
-        }
-    }
-    print $0
-    next
-}
-
-NR > 1 {
-    if (tax_col > 0) {
-        val = $(tax_col)
-        gsub(/[[:space:]]+/, "", val)
-
-        if (val ~ /^([0-9]+(\.[0-9]+)?|\.[0-9]+)$/) {
-            percent = (val + 0) * 100  
-            $(tax_col) = percent "%"
-        }
-        else if (val ~ /^([0-9]+(\.[0-9]+)?|\.[0-9]+)%$/) {
-            $(tax_col) = val
-        }
-        else {
-            $(tax_col) = "N/A"
-        }
-    }
-    print $0
-}
-
-' "$INPUT_FILE"
+INPUT_FILE="$1" 
+gawk script.awk "$INPUT_FILE"
